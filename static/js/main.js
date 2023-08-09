42b5b8ff0bd9ab6f5372e4fb385c9ee875418e3c
Tino B <bomantino@gmail.com>
2023-08-09 18:30:50 +0300

    fixed the js

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,17 @@
-document.getElementById("expenseForm").addEventListener("submit", function(event) {
+function validateExpenseDate() {
     var dateInput = document.getElementsByName("expense_date")[0];
     var datePattern = /^\d{4}-\d{2}-\d{2}$/;
 
     if (!datePattern.test(dateInput.value)) {
         alert("Invalid date format. Please use YYYY-MM-DD format.");
+        return false; // Prevent form submission
+    }
+
+    return true; // Allow form submission
+}
+
+document.getElementById("expenseForm").addEventListener("submit", function(event) {
+    if (!validateExpenseDate()) {
         event.preventDefault(); // Prevent form submission
     }
 });
